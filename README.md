3944320c19f8bf194c981bed285fc1834894c171
djumanov <diyorbekjumanov1120@gmail.com>
2023-02-10 14:40:14 +0500

    done

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# currency-converter
+# currency converter
+
+## objective
+
+This is a simple currency converter that converts from one currency to another
+
+## endpoints
+
+- `GET` `/api/to-usd` - converts from UZS (so'm) currency to USD
+- `GET` `/api/to-uzs` - converts from USD to UZS (so'm) currency
+
+## usage
+
+- `GET` `/api/to-usd?amount=1000` - converts 1000 UZS to USD
+
+### request
+  
+```http
+GET /api/to-usd?amount=1000
+```
+
+### response
+
+```json
+{
+  "amount": 1000,
+  "currency": "UZS",
+  "converted": 0.11,
+  "convertedCurrency": "USD"
+}
+```
+
+- `GET` `/api/to-uzs?amount=1000` - converts 1000 USD to UZS (so'm)
+
+### request
+  
+```http
+GET /api/to-uzs?amount=1000
+```
+
+### response
+
+```json
+{
+  "amount": 1000,
+  "currency": "USD",
+  "converted": 89200,
+  "convertedCurrency": "UZS"
+}
+```
---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+usd = 11380.7 # 1 USD = 11380.7 UZS
+
+@app.route('/api/to-usd', methods=['GET'])
+def to_usd():
+    """
+    Convert to USD
+
+    Returns:
+        json: Converted amount
+    
+    Note:
+        request data will be like this:
+            /api/to-usd?amount=1000
+        
+        response will be like this:
+            {
+                "amount": 1000,
+                "currency": "UZS",
+                "converted": 88.7,
+                "convertedCurrency": "USD"
+            }
+    """
+    pass
+
+@app.route('/api/to-uzs', methods=['GET'])
+def to_uzs():
+    """
+    Convert to UZS
+
+    Returns:
+        json: Converted amount
+    
+    Note:
+        request data will be like this:
+            /api/to-uzs?amount=1000
+        
+        response will be like this:
+            {
+                "amount": 1000,
+                "currency": "USD",
+                "converted": 1138070,
+                "convertedCurrency": "UZS"
+            }
+    """
+    pass
+    
+
+if __name__ == '__main__':
+    app.run()    
